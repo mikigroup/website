0d96e8caca3e343edf9f824afbee8726fa040334
nisarhassan12 <syednisarhassan12@gmail.com>
2022-07-19 17:13:36 +0500

    wip.

---
+++ src/lib/components/team/meet-the-team.svelte
@@ -0,0 +1,8 @@
+<script>
+  import SectionCommon from "../section-common.svelte";
+</script>
+
+<SectionCommon
+  title="Meet the team"
+  text="During our regular offsites we get to build a deeper bond and simply celebrate an amazing team and a great product."
+/>
---
+++ src/lib/contents/team.ts
@@ -0,0 +1,16 @@
+import type { Image } from "$lib/types/image.type";
+
+export const teamImages: Image[] = [
+  {
+    src: "/images/about/3.jpg",
+    alt: "Gitpod team members taking a selfie.",
+  },
+  {
+    src: "/images/about/1.png",
+    alt: "Gitpod team members taking a selfie.",
+  },
+  {
+    src: "/images/about/9.png",
+    alt: "Gitpod team members with surfing boards.",
+  },
+];
